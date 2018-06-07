c46000dba84b37a2a581244c99e4d7f25fcacf75
Hotboy <hotboyping@gmail.com>
2018-06-08 00:23:00 +0300

    Upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './style.less';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Suka Bleat!
+      </div>
+    );
+  }
+}
+
+export default App;
